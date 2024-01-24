741782e5bf85745e6ec303a3610a4fd589bf18a0
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-01-24 19:41:58 +0400

    lifespan

--- app/main.py
+++ app/main.py
@@ -1,20 +1,25 @@
 from fastapi import FastAPI
 
+from contextlib import asynccontextmanager
+
 from app.users import user_router
 from app.notes import note_router
 
 from app.database import init_db
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
+
+
 app = FastAPI(
-    title='FastAPI_Notes'
+    title='FastAPI_Notes',
+    lifespan=lifespan,
 )
 app.include_router(user_router)
 app.include_router(note_router)
-
-
-@app.on_event("startup")
-async def on_startup():
-    await init_db()
 
 
 # uvicorn app.main:app --reload --workers 1 --host 0.0.0.0 --port 8080
